19e5088cfdbc15273af94551dc49f56f6afd17d3
VITOR PAES RODRIGUES <24122327@dsn127m1242373.cfp127aluno.local>
2025-04-29 10:10:55 -0300

    segunda tela ok + terceira na metade

--- app/src/main/java/br/senai/sp/jandira/lionschool/screens/StudentsScreen.kt
+++ app/src/main/java/br/senai/sp/jandira/lionschool/screens/StudentsScreen.kt
@@ -1,59 +1,100 @@
 package br.senai.sp.jandira.lionschool.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.lionschool.R
+import br.senai.sp.jandira.lionschool.screens.components.CourseCircle
 
 @Composable
-fun StudentsScreen (){
+fun StudentsScreen() {
 
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .background(color = Color.White)
-            .padding(top=50.dp, bottom = 20.dp, start = 20.dp, end = 20.dp)
-    ){
-        Column (
+            .padding(top = 50.dp, bottom = 20.dp, start = 20.dp, end = 20.dp)
+    ) {
+        Column(
             modifier = Modifier
                 .fillMaxSize()
-        ){
-            Column (
+        ) {
+            Column(
                 verticalArrangement = Arrangement.SpaceBetween,
                 horizontalAlignment = Alignment.Start,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(200.dp)
-            ){
-                Column (
+                    .fillMaxHeight()
+            ) {
+                Column(
                     modifier = Modifier
                         .fillMaxWidth()
-                ){
+                ) {
                     Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
                         modifier = Modifier
                             .padding(bottom = 5.dp)
                             .fillMaxWidth()
                     ) {
-                        Image(
-                            painter = painterResource(id = R.drawable.logo_image),
-                            contentDescription = "Lion School Logo",
+                        Row (
                             modifier = Modifier
-                                .size(60.dp)
-                        )
+                        ){
 
-                        Text(
-                            text = stringResource(R.string.app_title),
-                            fontSize = 17.sp,
-                            fontWeight = FontWeight.Bold,
-                            color = Color(0xFF1262BE),
-                            modifier = Modifier
-                                .width(60.dp)
-                                .padding(4.dp),
+                            Image(
+                                painter = painterResource(id = R.drawable.logo_image),
+                                contentDescription = "Lion School Logo",
+                                modifier = Modifier
+                                    .size(60.dp)
+                            )
+                            Text(
+                                text = stringResource(R.string.app_title),
+                                fontSize = 17.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color(0xFF1262BE),
+                                modifier = Modifier
+                                    .width(60.dp)
+                                    .padding(4.dp),
+                            )
+                        }
+
+                        CourseCircle(
+                            courseAbbreviation = stringResource(R.string.ds_abbreviation)
                         )
                     }
 
@@ -64,12 +105,99 @@ fun StudentsScreen (){
                             .fillMaxWidth()
                     )
                 }
-    }
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    label = {
+                        Text(
+                            text = stringResource(R.string.find_student),
+                            color = Color(0xFF595959)
+                        ) },
+                    shape = RoundedCornerShape(8.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(45.dp),
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "",
+                            modifier = Modifier
+                                .size(24.dp)
+                        )
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number,
+                        imeAction = ImeAction.Next
+                    )
+                )
 
+                Row (
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ){
+                    Button(
+                        onClick = {},
+                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFFC23D)
+                        ),
+                        shape = RoundedCornerShape(30.dp),
+                        modifier = Modifier
+                            .width(90.dp)
+                            .height(36.dp)
+                    ) {
+                        Text(
+                            text = stringResource(R.string.all_students),
+                            fontSize = 13.sp,
+                            color = Color(0xFF1262BE)
+                        )
+                    }
+
+                    Button(
+                        onClick = {},
+                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFFC23D)
+                        ),
+                        shape = RoundedCornerShape(30.dp),
+                        modifier = Modifier
+                            .width(110.dp)
+                            .height(36.dp)
+                    ) {
+                        Text(
+                            text = stringResource(R.string.studying_students),
+                            fontSize = 13.sp,
+                            color = Color(0xFF1262BE)
+                        )
+                    }
+
+                    Button(
+                        onClick = {},
+                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFFC23D)
+                        ),
+                        shape = RoundedCornerShape(30.dp),
+                        modifier = Modifier
+                            .width(110.dp)
+                            .height(36.dp)
+                    ) {
+                        Text(
+                            text = stringResource(R.string.graduate_students),
+                            fontSize = 13.sp,
+                            color = Color(0xFF1262BE)
+                        )
+                    }
+                }
+
+                Row (
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ){
+
+                }
+            }
+        }
+    }
 }
 
 @Preview(showSystemUi = true)
 @Composable
-private fun StudentsScreenPreview() {
+private fun CourseScreenPreview() {
     StudentsScreen()
 }
